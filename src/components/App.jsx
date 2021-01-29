e5a86e56edcbee231537cdc6cb27dff9f8373184
lmwang27 <52217891+lmwang27@users.noreply.github.com>
2021-01-29 08:20:21 -0800

    adding spread operator

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState(["A Item"]);
+
+  function handleChange(event) {
+    setInputText(event.target.value);
+  }
+  function handleClick(event) {
+    setItems((preValue) => [...preValue, inputText]);
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input type="text" onChange={handleChange} value={inputText} />
+        <button onClick={handleClick}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {items.map((item) => (
+            <li>{item}</li>
+          ))}
         </ul>
       </div>
     </div>
